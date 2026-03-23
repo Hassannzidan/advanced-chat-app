23edeb91464a0d873e75b250f67ee4ab826623d6
Hassan Zidan <hzidan014@gmail.com>
2026-03-23 22:09:57 +0200

    feat: implement error handling middleware for centralized error management in Express

---
+++ Backend/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,29 @@
+import { ErrorRequestHandler } from "express";
+import { HTTPSTATUS } from "../config/http.config";
+import { AppError, ErrorCodes } from "../utils/app-error";
+
+
+export const errorHandler: ErrorRequestHandler = (
+    err, 
+    req, 
+    res, 
+    next
+): any => {
+    console.log(`Error occurred: ${err.message} at ${req.originalUrl} on ${req.method}, path: ${req.path}`);
+    
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message,
+            error: err.errorCode,
+            timestamp: new Date().toISOString(),
+        });
+    }
+    
+    
+    return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+        message: "Internal Server Error",
+        error: err.message,
+        errorCode: ErrorCodes.ERR_INTERNAL,
+        timestamp: new Date().toISOString(),
+    });
+}
