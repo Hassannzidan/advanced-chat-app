9d8ec607149b4814eabf134b592cf7e5bcff469b
Hassan Zidan <hzidan014@gmail.com>
2026-03-24 09:11:30 +0200

    feat: add utility functions for setting and clearing JWT authentication cookies

---
+++ Backend/src/utils/cookie.ts
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import type { Response } from "express";
+import { envConfig } from "../config/env.config";
+
+type Cookie = {
+    res: Response;
+    userId: string; // User ID
+}
+
+export const setJwtAuthCookie = ({res, userId}: Cookie) => {
+   const payload = { userId };
+   const expiresIn = envConfig.JWT_EXPIRES_IN;
+   const token = jwt.sign(payload, envConfig.JWT_SECRET, { 
+    audience:["user"],
+    expiresIn: expiresIn || "7d",
+ });
+
+ return res.cookie("authToken", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: envConfig.NODE_ENV === "production" ? true : false,
+    sameSite: envConfig.NODE_ENV === "production" ? "strict" : "lax",
+  });
+}
+
+export const clearJwtAuthCookie = (res: Response) => 
+ res.clearCookie("authToken", { path: "/" });
