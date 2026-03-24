21b1a7f72ebb71258f44f652ce0aa19a92d5f77d
Hassan Zidan <hzidan014@gmail.com>
2026-03-24 18:03:10 +0200

    refactor: rename authRouter to authRoutes for consistency in routing

--- Backend/src/routes/auth.route.ts
+++ Backend/src/routes/auth.route.ts
@@ -2,10 +2,10 @@ import { Router } from "express";
 import { logoutController, loginController, registerController, authStatusController } from "../controllers/auth.controller";
 import { passportAuthenticateJwt } from "../config/passport.config";
 
-const authRouter = Router()
+const authRoutes = Router()
     .post("/register", registerController)
     .post("/login", loginController)
     .post("/logout", logoutController)
     .get("/status", passportAuthenticateJwt, authStatusController);
     
-export default authRouter;
+export default authRoutes;
