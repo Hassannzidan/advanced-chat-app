4dc9c396da227cbe19d3ebcc5c37e4176404f915
Hassan Zidan <hzidan014@gmail.com>
2026-03-24 14:36:17 +0200

    feat: integrate Passport for authentication middleware initialization

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,11 +2,13 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import passport from "passport";
 import { envConfig } from "./config/env.config";
 import { HTTPSTATUS } from "./config/http.config";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 import connectDatabase from "./config/database.config";
+import "./config/passport.config";
 
 const app = express();
 app.use(express.json());
@@ -16,6 +18,7 @@ app.use(cors({
     origin: envConfig.FRONTEND_ORIGIN,
     credentials: true,
 }));
+app.use(passport.initialize());
 
 app.get("/health", 
     asyncHandler(async (req: Request, res: Response) => {
