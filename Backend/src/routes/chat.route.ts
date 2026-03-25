bd44c1edf51c474765f4c06ce836ac4b066e4388
Hassan Zidan <hzidan014@gmail.com>
2026-03-25 09:54:03 +0200

    feat: add endpoint for sending messages in chat routes

--- Backend/src/routes/chat.route.ts
+++ Backend/src/routes/chat.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { passportAuthenticateJwt } from "../config/passport.config";
 import { createChatController, getUsersChatsController, getSingleChatController } from "../controllers/chat.controller";
+import { sendMessageController } from "../controllers/message.controller";
 
 const chatRoutes = Router()
     .use(passportAuthenticateJwt)
     .post("/create", createChatController)
+    .post("/send-message", sendMessageController)
     .get("/all", getUsersChatsController)
     .get("/:id", getSingleChatController)
     
